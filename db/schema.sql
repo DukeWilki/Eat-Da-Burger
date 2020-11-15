a3e427ed1c0cbe279d213bbe39aa7925bc6169de
DukeWilki <65857753+DukeWilki@users.noreply.github.com>
2020-11-15 22:14:20 +1100

    formatting

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-	rating INT(2) NOT NULL,
+	rating INT(2) NOT NULL DEFAULT 99,
 	eaten BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,5 +2,5 @@ INSERT INTO burgers (name) VALUES ('Fig Mac');
 INSERT INTO burgers (name) VALUES ('Double Flopper');
 INSERT INTO burgers (name) VALUES ('Quadruple Pounder');
 INSERT INTO burgers (name) VALUES ('Fill of Fish Sticks');
-INSERT INTO burgers (name) VALUES ('KFC Zinca');
+INSERT INTO burgers (name) VALUES ('Green Eggs and Hamburger');
 INSERT INTO burgers (name) VALUES ('Handburger');
